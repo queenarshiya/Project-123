5a37e9859a65e0099c4eca93059f234174d4fa57
queenarshiya <98427385+queenarshiya@users.noreply.github.com>
2023-07-13 17:36:30 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function_preload(){
+
+}
+
+function_setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+
+}
+
+function_draw(){
+
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
